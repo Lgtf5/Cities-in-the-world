353e0961c0629d53257248300852ebf5d63b2eb7
Lgtf5 <lgtablasf5@gmail.com>
2025-01-20 18:56:04 +0100

    template layers footer, bottom-grid, left-grid, right-grid, navbar & 1-all-template

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
       backgroundImage: {
         'HeadCity': "url('../src/7b768ed9-3e35-465d-aef7-b8eac97037ba_alta-aspect-ratio_default_0-723601349.jpg')",
         'restaurant': "url('../src/restaurant.jpg')",
+        'Site1': "url('../src/brujas-principal-3165827918.jpg')",
+        'Site2': "url('../src/medieval-houses-in-market-2821109251.jpg')",
+        'Site3': "url('..')",
+        'Site4': "url('..')",
+        'Site5': "url('..')",
+        'Site6': "url('../src/que-visitar-brujas-belgica-1719545065.jpg')",
+        'icon': "url('../src/pngwing.com.png')",
+
+
+
+
       }
     },
   },
